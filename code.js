c30e0b9055cb72a08708a7a890dab457e461d396
Utkir <utkirkurbanov2700@gmail.com>
2022-10-12 10:27:38 +0500

    1-st commit

---
+++ code.js
@@ -0,0 +1,9 @@
+var num = 2.6;
+
+console.log(Math.random());
+
+console.log(Math.floor(num)); //rounds the number down the side
+console.log(Math.ceil(num)); ////rounds the number up the side
+
+var count = Math.PI;
+console.log(count * 2 * 5);
